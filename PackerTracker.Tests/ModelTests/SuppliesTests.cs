994a37987507663fd74d886d733427bd19f8c361
unknown <macjay314@gmail.com>
2020-05-20 15:13:13 -0700

    Create and test Supplies Models.

---
+++ PackerTracker.Tests/ModelTests/SuppliesTests.cs
@@ -0,0 +1,25 @@
+// using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PackerTracker.Models;
+
+namespace PackerTracker.Tests
+{
+  [TestClass]
+  public class SuppliesTests
+  {
+    [TestMethod]
+    public void SuppliesConstructor_CreatesInstanceOfSupplies_Supplies()
+    {
+      string description = "this is something you eat";
+      Supplies supply = new Supplies("food", description, 2, 100);
+
+      string nameTest = "food";
+      int priceTest = 2, idTest = 100;
+
+      Assert.AreEqual(nameTest, supply.Name);
+      Assert.AreEqual(description, supply.Description);
+      Assert.AreEqual(priceTest, supply.Price);
+      Assert.AreEqual(idTest, supply.ID);
+    }
+  }
+}
---
+++ PackerTracker/Models/Supplies.cs
@@ -0,0 +1,17 @@
+namespace PackerTracker.Models
+{
+  public class Supplies
+  {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public int ID { get; set; }
+    public Supplies(string name, string description, int price, int id)
+    {
+      Name = name;
+      Description = description;
+      Price = price;
+      ID = id;
+    }
+  }
+}
